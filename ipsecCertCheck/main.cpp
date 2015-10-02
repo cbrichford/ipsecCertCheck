fcd53249bb1e86f5efe3a8416edd8f0cf6dffc8e
Christopher Alva Brichford <cbrichford@users.noreply.github.com>
2015-10-02 16:56:37 -0700

    Initial commit

---
+++ ipsecCertCheck/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ipsecCertCheck
+//
+//  Created by Christopher Brichford on 9/28/15.
+//  Copyright © 2015 Christopher Brichford. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
